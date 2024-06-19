d7526ced64afb015f7b76a04766d739abd76170d
Joel Thomas <joelthomas1103@gmail.com>
2024-06-19 07:47:48 +0530

    new changes done

--- script.js
+++ script.js
@@ -9,3 +9,16 @@ email.addEventListener('focus',()=>{
 email.addEventListener('blur',()=>{
     label.classList.remove('active')
 })
+
+
+
+let mail2 = document.getElementById('email2')
+let label2 = document.getElementById('email-label2')
+mail2.addEventListener('focus',()=>{
+    label2.classList.add('active')
+})
+
+mail2.addEventListener('blur',()=>{
+    label2.classList.remove('active')
+    
+})
